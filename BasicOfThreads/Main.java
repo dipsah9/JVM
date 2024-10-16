22ad7b07712553bd1978525fea3b8955ab252b8f
Dipendra Sah <dipsah9@gmail.com>
2024-10-16 11:01:49 +0200

    Threads

---
+++ BasicOfThreads/Main.java
@@ -0,0 +1,25 @@
+package BasicOfThreads;
+
+ class MyThreads extends Thread{
+
+    @Override
+    public  void run(){
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i + "from Thread " + this.getName());
+        }     
+    }
+
+}
+
+public class Main{
+    public static void main(String[] args) {
+        MyThreads thread1 = new MyThreads();
+        thread1.start();
+
+        MyThreads threads2 = new MyThreads();
+        threads2.start();
+      
+    }
+}
+   
+
